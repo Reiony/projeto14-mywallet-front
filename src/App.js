ad26f2d18c3bccef039ee84e9a17151b76fe43ee
Rennan <reiony.dev@gmail.com>
2023-01-24 23:22:48 -0300

    feat: added new entry and exit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles.js";
 import SignIn from "./components/SignIn.js"
 import SignUp from "./components/SignUp.js";
-
+import NewEntry from "./components/NewEntry.js";
+import Home from "./components/Home.js"
+import NewExit from "./components/NewExit.js";
 export default function App() {
   return (
     <>
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Home />}/> */}
+          <Route path="/" element={<Home />}/>
           <Route path="sign-up" element={<SignUp/>}/>
           <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="new-entry" element={<NewEntry/>}/>
+          <Route path="/new-exit" element={<NewExit/>}/>
         </Routes>
       </BrowserRouter>
     </>
